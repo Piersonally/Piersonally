4a29bfafcf1043a26bcdb1cac8eb3e366ef1fd35
Sam Pierson <sam@sampierson.com>
2014-02-09 08:56:34 -0800

    bootstrapify layout

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,17 @@
+module LayoutHelper
+
+  def site_title_helper
+    'Piersonally' + (Rails.env == 'production' ? '' : " #{Rails.env}")
+  end
+
+  def page_heading_helper
+    if content_for? :heading
+      content_for :heading
+    elsif @heading == false
+      # Suppress heading
+    else
+      heading = @heading || title || 'MISSING HEADING, SET title OR content_for :heading'
+      content_tag :h1, heading
+    end
+  end
+end
