d0bfe26a34e8eb96630ebe8141b6cdfa909d07f7
Sam Pierson <sam@sampierson.com>
2014-06-04 10:13:22 -0700

    Update wow -> 0.1.5 (inverse, last_snapshot)

---
+++ db/migrate/20140604170009_unique_index_in_item_blizz_id.wow_engine.rb
@@ -0,0 +1,8 @@
+# This migration comes from wow_engine (originally 20140529033158)
+class UniqueIndexInItemBlizzId < ActiveRecord::Migration
+  def change
+    remove_index :wow_items, :blizz_item_id
+    add_index :wow_items, :blizz_item_id, unique: true
+    add_foreign_key :wow_auctions, :wow_items, column: 'item_id'
+  end
+end
