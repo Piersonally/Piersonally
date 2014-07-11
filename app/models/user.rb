e3563eb91228f0429175ba13e9b999cf669d0c1b
Sam Pierson <sam@sampierson.com>
2014-07-10 19:11:24 -0700

    Turn of User devise validatable and other unused modules.
    Looks like we have to keep database_authenticatable, otherwise routes disappear.
    destroy_user_session_path disappeared.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :trackable, :validatable
 
-  devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauth_providers => [:google_oauth2]
+  devise :database_authenticatable, :trackable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
   def self.from_omniauth(auth)
     if user = User.find_by_email(auth.info.email)
