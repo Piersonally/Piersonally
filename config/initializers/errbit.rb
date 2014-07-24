2b42631100c9135bc3583ce34efba2d99bbec4f5
Sam Pierson <sam@sampierson.com>
2014-07-23 17:34:08 -0700

    Configure errbit (http only)

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,6 @@
+Airbrake.configure do |config|
+  config.api_key = 'c2a62165b44c44b9c57e0c30136400ab'
+  config.host    = 'x.piersonally.com'
+  config.port    = 80
+  config.secure  = config.port == 443
+end
